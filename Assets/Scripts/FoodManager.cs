7e993d4f110fc3bb08fe2e07e2f42366a362e23c
QZQTechMonster <928221136@qq.com>
2018-06-01 22:06:05 +0800

    [ADD] food manager

--- Assets/Scripts/FoodManager.cs
+++ Assets/Scripts/FoodManager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class FoodManager : MonoBehaviour {
 
-	public GameObject[] foods;
+
+    public GameObject[] foodsCandidates;
+    public Transform[] lootPoints;
+
+    private GameObject[] foods;
+    //private float[] 
+    private int numOfFoods;
 
 	// Use this for initialization
 	void Start () {
-		
+        numOfFoods = lootPoints.Length;
+        foods = new GameObject[numOfFoods];
+        for (int i = 0; i < numOfFoods; ++i) {
+            foods[i] = Instantiate(foodsCandidates[(int)Random.Range(0, foodsCandidates.Length)],
+                                   lootPoints[i].position, 
+                                   Quaternion.identity); 
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
+        for (int i = 0; i < numOfFoods; ++i)
+        {
+            if(foods[i] == null) {
+                foods[i] = Instantiate(foodsCandidates[(int)Random.Range(0, foodsCandidates.Length)],
+                                   lootPoints[i].position,
+                                   Quaternion.identity);    
+            }
+        }
 	}
 }
