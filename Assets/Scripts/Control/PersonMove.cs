edf9c008770ec742f2922cf038587504d25679c4
Isabella98Liu <isabellaliu98@berkeley.edu>
2018-06-02 04:09:46 +0800

    【ADD】Role rotate

--- Assets/Scripts/Control/PersonMove.cs
+++ Assets/Scripts/Control/PersonMove.cs
@@ -8,13 +8,25 @@ public class PersonMove : MonoBehaviour {
 
     public float Scale = 50.0f;
 
+    private GameObject Player;
+    private Vector2 lastFrame;
+
 	// Use this for initialization
 	void Start () {
-
+        Player = GameObject.Find("Player/Stick");
 	}
 	
 	// Update is called once per frame
 	void Update () {
         this.GetComponent<Rigidbody2D>().AddForce(new Vector2(Input.GetAxis("Horizontal"), 0) * Scale);
+        Vector2 velocity;
+        velocity = this.GetComponent<Rigidbody2D>().velocity.normalized;
+        if (velocity.x * lastFrame.x < 0)
+            Player.transform.Rotate( new Vector3(180, 0, 0));
+
+    }
+    private void LateUpdate()
+    {
+        lastFrame = this.GetComponent<Rigidbody2D>().velocity.normalized;
     }
 }
