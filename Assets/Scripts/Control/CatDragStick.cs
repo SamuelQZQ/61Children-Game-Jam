7eed1327dfcd966639836321b2db5686b221d75d
QZQTechMonster <928221136@qq.com>
2018-06-01 20:59:30 +0800

    [ADD] mouse drag component

---
+++ Assets/Scripts/Control/CatDragStick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatDragStick : MonoBehaviour {
+
+    public float maxForce, scale;
+    public Transform dragTransform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0)) {
+            
+			Vector3 mousePositionOnScreen = Input.mousePosition;  
+			Vector3  mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePositionOnScreen);  
+            mousePositionInWorld.z = transform.position.z;
+
+            Vector3 dragForce = dragTransform.position - mousePositionInWorld;
+            dragForce *= scale;
+			if(dragForce.magnitude > maxForce) {
+				dragForce =  dragForce.normalized * maxForce;
+			}
+
+            GetComponent<Rigidbody2D>().AddForceAtPosition(dragForce, dragTransform.position);
+		}		
+	}
+	
+	
+}
---
+++ Assets/Scripts/FoodManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodManager : MonoBehaviour {
+
+	public GameObject[] foods;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
