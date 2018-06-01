dc635b9da2b1c6666b31aff9f642b186cb0db8e2
Isabella98Liu <isabellaliu98@berkeley.edu>
2018-06-01 20:10:24 +0800

    【ADD】Player prefab

---
+++ Assets/Scripts/PersonMove.cs
@@ -0,0 +1,21 @@
+﻿// this script should be attached to the palyer's role
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonMove : MonoBehaviour {
+
+    public float Scale = 50.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(Input.GetAxis("Horizontal"));
+        this.GetComponent<Rigidbody2D>().AddForce(new Vector2(Input.GetAxis("Horizontal"), 0) * Scale);
+    }
+}
