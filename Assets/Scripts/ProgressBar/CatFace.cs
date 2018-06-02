012eafa7287fd8bb5e1ac434c1f2d57662dff6a9
Isabella98Liu <isabellaliu98@berkeley.edu>
2018-06-03 02:43:07 +0800

    【ADD】Progress bar & cat face animation

---
+++ Assets/Scripts/ProgressBar/CatFace.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CatFace : MonoBehaviour {
+
+    public Sprite hungry100;
+    public Sprite hungry70;
+    public Sprite hungry30;
+    public int CatType;
+
+    private float hungry;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CatType == 1)
+            hungry = Global.HungryDegree1;
+        else if (CatType == 2)
+            hungry = Global.HungryDegree2;
+
+        if (hungry > 70)
+            this.GetComponent<Image>().sprite = hungry100;
+        else if (hungry <= 70 && hungry > 30)
+            this.GetComponent<Image>().sprite = hungry70;
+        else
+            this.GetComponent<Image>().sprite = hungry30;
+    }
+}
--- Assets/Scripts/ProgressBar/ProgressBar.cs
+++ Assets/Scripts/ProgressBar/ProgressBar.cs
@@ -7,8 +7,6 @@ public class ProgressBar : MonoBehaviour {
 
     public GameObject Hungry1;
     public GameObject Hungry2;
-    public GameObject FoodCount1;
-    public GameObject FoodCount2;
 
 	// Use this for initialization
 	void Start () {
@@ -19,8 +17,6 @@ public class ProgressBar : MonoBehaviour {
     void Update()
     {
         Hungry1.GetComponent<Image>().fillAmount = (float)Global.HungryDegree1 / 100;
-        FoodCount1.GetComponent<Image>().fillAmount = (float)Global.FoodCount1 / 50;
         Hungry2.GetComponent<Image>().fillAmount = (float)Global.HungryDegree2 / 100;
-        FoodCount2.GetComponent<Image>().fillAmount = (float)Global.FoodCount2 / 50;
     }
 }
