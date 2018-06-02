7665a444b041754f2dfd0329751eabcbf0b0eceb
Isabella98Liu <isabellaliu98@berkeley.edu>
2018-06-02 15:47:14 +0800

    【FIX】Anchor point &midpoint

--- Assets/Scripts/Control/StickLength.cs
+++ Assets/Scripts/Control/StickLength.cs
@@ -8,25 +8,27 @@ public class StickLength : MonoBehaviour {
 
     public float MaxOffset = 1.0f;
     public float Scale = 0.1f;
-    public GameObject Person;
+    public GameObject Anchor;
+    public GameObject MidPoint;
 
     private float Dis;
     private float oldDis;
+    private float oldZ;
     private Vector3 dir;
-    private Vector3 AnchorPos;
 
 	// Use this for initialization
 	void Start () {
-        AnchorPos = new Vector2(Person.transform.position.x, Person.transform.position.y) + Person.GetComponent<HingeJoint2D>().anchor;
-        oldDis = (this.transform.position - AnchorPos).magnitude;
+        oldDis = (MidPoint.transform.position - Anchor.transform.position).magnitude;
+        oldZ = this.transform.position.z;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        AnchorPos = new Vector2(Person.transform.position.x, Person.transform.position.y) + Person.GetComponent<HingeJoint2D>().anchor;
-        Dis = (this.transform.position - AnchorPos).magnitude;
-        dir = this.transform.position - AnchorPos;
-        Debug.DrawLine(this.transform.position, AnchorPos, Color.green);
+        Dis = (MidPoint.transform.position - Anchor.transform.position).magnitude;
+        dir = MidPoint.transform.position - Anchor.transform.position;
+        dir.z = 0;
+        Debug.DrawLine(MidPoint.transform.position, Anchor.transform.position, Color.green);
+
         if (Input.GetAxis("Vertical") > 0 && (Dis - oldDis <= MaxOffset))   // if the up button was pressed
         {
             this.transform.position += dir * Scale;
