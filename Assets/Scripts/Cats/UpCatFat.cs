96bf23f2a7cea4887d1e1c8687ebb4195dcfe0ab
QZQTechMonster <928221136@qq.com>
2018-06-02 13:48:24 +0800

    cat can change size

---
+++ Assets/Scripts/Cats/UpCatFat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpCatFat : MonoBehaviour {
+
+    public UpCat upCat;
+    Animator animator;
+
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+        animator.SetInteger("Weight", upCat.weight);
+	}
+}
